770550326e22edb30755aab01ca4254b7ba3982d
Samantha Crespo <samkcrespo@gmail.com>
2022-03-23 15:27:52 -0700

    Adding elements to create website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Software Engineering Student: Always learning";
 
 document.querySelector("body").appendChild(h2);

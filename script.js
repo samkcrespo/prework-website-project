10dc33d10a17ce382e75824cf954871c46ef9891
Samantha Crespo <samkcrespo@gmail.com>
2022-03-23 15:57:35 -0700

    updating body bg color & text color

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Software Engineering Student: Always learning";
+h2.textContent = "Software Engineering Student";
 
 document.querySelector("body").appendChild(h2);
